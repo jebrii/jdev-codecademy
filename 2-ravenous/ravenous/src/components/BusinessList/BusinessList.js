8bf8d74fbd06e7b165dbe0a0fc177891a78d16ec
jebri <henry@hraschke.com>
2018-09-22 13:59:24 -0600

    Finished ravenous III

--- 2-ravenous/ravenous/src/components/BusinessList/BusinessList.js
+++ 2-ravenous/ravenous/src/components/BusinessList/BusinessList.js
@@ -4,14 +4,14 @@ import Business from '../Business/Business.js'
 
 class BusinessList extends React.Component {
   render() {
+
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {
+          this.props.businesses.map(business => {
+            return (<Business business={business} />);
+          })
+        }
       </div>
     );
   }
