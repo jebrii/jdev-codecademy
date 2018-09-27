204f51a7f5f558dd6b0f07aa8101362a0f132460
jebri <henry@hraschke.com>
2018-09-27 13:19:17 -0700

    initial jammming commit

---
+++ 3-jammming/jammming/src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from '../../assets/logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <span></span>
+    );
+  }
+}
+
+export default App;
